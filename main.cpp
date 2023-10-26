88e454de46dbec3baeb33a1f861c17065e89cfef
Kujakuh <avos202@gmail.com>
2023-10-26 15:51:20 +0200

    Multiple cubes render. 1 plane moving camera

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp> // For glm::ortho
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <SHADER/shader_s.h>
 #include <TEXTURE/texture_s.h>
@@ -18,6 +18,7 @@ constexpr auto _HEIGHT = 600;
 constexpr auto _WIDTH = 800;
 
 bool wireframeOn = false;
+mat4 model = mat4(1.0f), view = mat4(1.0f), projection = mat4(1.0f);
 
 #pragma region FPS_COUNTER
 double previousTime = 0;
@@ -34,8 +35,9 @@ void updateDeltaTime();
 void inputManagement(GLFWwindow* window);
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
-
+float FOV = 45.0f;
 float swap_var = 0.2f;
+float cameraSpeed = 2.5f;
 
 int main()
 {
@@ -68,6 +70,8 @@ int main()
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     #pragma endregion "GLFW window and Glad (OpenGL) setup"
 
     GLenum texture_config[4] = { GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR };
@@ -77,27 +81,86 @@ int main()
 
     Shader main_shader("shaders/vShader.vert", "shaders/fShader.frag");
 
-    mat4 trans = mat4(1.0f);
-    trans = scale(trans, vec3(0.5f, 0.5f, 1.0f));
-    trans = translate(trans, vec3(0.5f, -0.5f, 0.0f));
 
-
-    float triangleVertices[] = {
+    /*float triangleVertices[] = {
          // positions           // colors           // texture coords
          0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,   1.0f, 1.0f,         // top right
          0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // bottom right
         -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,   0.0f, 0.0f,         // bottom left
         -0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 0.0f,   0.0f, 1.0f          // top left
     };
-
+    
     GLuint indices[] = {
-        0, 1, 3,    // first triangle
-        1, 2, 3     // second triangle
+            0, 1, 3,    // first triangle
+            1, 2, 3     // second triangle
     };
 
-    GLuint VAO, VBO, EBO;               // Vertex array object, Vertex buffer object, Element buffer object
+    */
+
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    vec3 cubePositions[] = {
+        vec3( 0.0f, 0.0f, 0.0f),
+        vec3( 2.0f, 5.0f, -15.0f),
+        vec3(-1.5f, -2.2f, -2.5f),
+        vec3(-3.8f, -2.0f, -12.3f),
+        vec3( 2.4f, -0.4f, -3.5f),
+        vec3(-1.7f, 3.0f, -7.5f),
+        vec3( 1.3f, -2.0f, -2.5f),
+        vec3( 1.5f, 2.0f, -2.5f),
+        vec3( 1.5f, 0.2f, -1.5f),
+        vec3(-1.3f, 1.0f, -1.5f)
+    };
+
+
+    GLuint VAO, VBO;                    // Vertex array object, Vertex buffer object, Element buffer object
     glGenVertexArrays(1, &VAO);         // Generate the buffer array for VAO
     glGenBuffers(1, &VBO);              // Generate the buffer for VBO
+
+/*  
+    GLuint EBO;                         
     glGenBuffers(1, &EBO);              // Generate the buffer for EBO
 
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes
@@ -113,11 +176,11 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    /*
-     GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
-     GL_STATIC_DRAW: the data is set only once and used many times.
-     GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
-    */
+    
+     //GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+     //GL_STATIC_DRAW: the data is set only once and used many times.
+     //GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -125,16 +188,32 @@ int main()
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
+*/
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
 
     // Unbinding VAO, VBO AND EBO
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     main_shader.use();
     main_shader.setIntUniform("ourTexture0", container.getTextureUnit());
     main_shader.setIntUniform("ourTexture1", face.getTextureUnit());
+    
+    view = translate(view, vec3(0.0f, 0.0f, -3.0f));
+    
 
     #pragma region MAIN_RENDER_LOOP
     while (!glfwWindowShouldClose(window))
@@ -143,24 +222,37 @@ int main()
         inputManagement(window);
 
         glClearColor(0.05f, 0.62f, 0.62f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        main_shader.use();
-        container.bind();
-        face.bind();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         fps();
 
+        container.bind();
+        face.bind();
+        main_shader.use();
+
+        projection = perspective(radians(FOV), (float)_WIDTH / (float)_HEIGHT, 0.1f, 100.0f);
+        main_shader.setMat4Uniform("view", view);
+        main_shader.setMat4Uniform("projection", projection);
+
         main_shader.setFloatUniform("pick", swap_var);
-
-        trans = rotate(trans, (float) sin(glfwGetTime()) * deltaTime, vec3(0, 0, 1));
-        main_shader.setMat4Uniform("model", trans);
-
+        
         glBindVertexArray(VAO);
-        // Mode, num of vertices, data type of the indices, and offset 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // Mode, starting index, num of vertices
-        // glDrawArrays(GL_TRIANGLES, 0, 6); // Without EBO
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            float angle = glfwGetTime() * -1 * i;
+            if(i%2==0) angle = glfwGetTime() + i*2;
+            
+            model = mat4(1.0f);
+            model = translate(model, cubePositions[i]);
+            model = rotate(model, angle, vec3(1.0f*i, 0.3f*i, 0.5f*i));
+            main_shader.setMat4Uniform("model", model);
+    
+            // Mode, num of vertices, data type of the indices, and offset 
+            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+            // Mode, starting index, num of vertices
+            glDrawArrays(GL_TRIANGLES, 0, 36); // Without EBO
+        }
         glBindVertexArray(0);
 
         // Double buffer swapping and event catching
@@ -172,7 +264,7 @@ int main()
     // De-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    //glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
@@ -187,9 +279,11 @@ void fps()
     // If a second has passed.
     if (currentTime - previousTime >= 1.0)
     {
+        system("cls");
         // Display the frame count here any way you want.
-        cout << frameCount << endl;
-
+        cout << "FPS: " << frameCount << endl;
+        cout << "Delta time: " << deltaTime << endl;
+        cout << "FOV: " << FOV << endl;
         frameCount = 0;
         previousTime = currentTime;
     }
@@ -204,8 +298,11 @@ void updateDeltaTime()
 
 void inputManagement(GLFWwindow* window)
 {
+    #pragma region ESC_CLOSE_WINDOW
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    #pragma endregion
+    #pragma region WIRE_FRAME
     if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
     {
         if (wireframeOn) {
@@ -218,6 +315,8 @@ void inputManagement(GLFWwindow* window)
             wireframeOn = true;
         }
     }
+    #pragma endregion
+    #pragma region TEXTURE_SWAP
     if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
     {
         swap_var += deltaTime;
@@ -228,7 +327,30 @@ void inputManagement(GLFWwindow* window)
         swap_var -= deltaTime;
         if(swap_var < 0.0f) swap_var = 0.0f;
     }
-}
+    #pragma endregion
+    #pragma region CAMERA_MOVEMENT
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        view = translate(view, vec3(0.0f, 0.0f, cameraSpeed*2*deltaTime));
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        view = translate(view, vec3(0.0f, 0.0f, -cameraSpeed*2*deltaTime));
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        view = translate(view, vec3(0.0f, -cameraSpeed*deltaTime, 0.0f));
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        view = translate(view, vec3(0.0f, cameraSpeed*deltaTime, 0.0f));
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        view = translate(view, vec3(cameraSpeed*deltaTime, 0.0f, 0.0f));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        view = translate(view, vec3(-cameraSpeed*deltaTime, 0.0f, 0.0f));
+    #pragma endregion
+    #pragma region FOV
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        FOV += 20.0f*deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        FOV -= 20.0f*deltaTime;
+    if (FOV > 100.0f) FOV = 100.0f;
+    if (FOV < 45.0f) FOV = 45.0f;
+    #pragma endregion
+}   
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
--- shaders/vShader.vert
+++ shaders/vShader.vert
@@ -1,17 +1,20 @@
 #version 460 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTextCoord;
+//layout (location = 1) in vec3 aColor;
+layout (location = 1) in vec2 aTextCoord;
 
-out vec3 ourColor;
+//out vec3 ourColor;
 out vec2 textCoord;
 
 uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 
 void main()
 {
-    gl_Position = model * vec4(aPos, 1.0);
-    ourColor = aColor;
-    textCoord = aTextCoord;
+    gl_Position = projection  * view * model * vec4(aPos, 1.0);
+    //ourColor = aColor;
+    textCoord = vec2(aTextCoord.x, aTextCoord.y);
 };
